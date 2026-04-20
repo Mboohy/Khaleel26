d10e6da757417cd7b941c8fe1602d02a4f271e85
Mboohy <mostafaelbohy96@gmail.com>
2026-04-20 13:56:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ END_DATE = "2026-12-31"
 if datetime.now().strftime('%Y-%m-%d') > END_DATE:
     sys.exit(0)
 
-INSTITUTE_NAME = "khaleel"
+INSTITUTE_NAME = "Khalil"
 BASE_URL = "https://api.khalilfiqh.com"
 LOGIN_URL = f"{BASE_URL}/api/login"
 GOOGLE_SHEET_ID = "10BT1405GJLNm1sY_3zCpP9mTb7Q6xHQRvEjBiAv-QEA"
